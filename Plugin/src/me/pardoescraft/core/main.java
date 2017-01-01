30acf7f7160a68c7f51a52ab259ee7fbe42aea10
BuildTools <unconfigured@null.spigotmc.org>
2017-01-01 16:44:07 +0100

    Nieuw!

---
+++ Plugin/src/me/pardoescraft/core/main.java
@@ -0,0 +1,17 @@
+package me.pardoescraft.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by MichelFIX on 01/01/2017.
+ */
+public class main extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+}
